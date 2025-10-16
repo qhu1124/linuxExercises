37728c065c86ce939a4afe75f47169f4804de55a
Ani Shi <hshi248@wisc.edu>
2025-10-16 13:21:48 -0500

    Add five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+rm -rf five
+mkdir five
+
+for i in {1..5}; do
+  dir="five/dir$i"
+  mkdir -p "$dir"
+
+  for j in {1..4}; do
+    file="$dir/file$j"
+    yes "$j" | head -n "$j" > "$file"
+  done
+done
+
+echo "Directory structure created successfully!"
