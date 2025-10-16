94d76c1c338944c8cd30508e4d6a4577d3ec49df
Qixuan Hu <qhu86@wisc.edu>
2025-10-16 13:03:51 -0500

    add school.sh to compute average TotalAssessedValue

--- school.sh
+++ school.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 | \
+{ 
+  sum=0; 
+  count=0; 
+  while read value; do 
+    sum=$((sum + value)); 
+    count=$((count + 1)); 
+  done; 
+  echo "average totalAssessedValue for properties in MADISON SCHOOLS = $((sum / count))"; 
+}
