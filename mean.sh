dc73b8fc746f8f29a1a1c3e4e962ae457aa37c42
Anniezhang0402 <xzhang2842@wisc.edu>
2025-10-16 13:37:41 -0500

    add mean.sh

--- mean.sh
+++ mean.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 
+if [ "$#" -lt 1 ]; then
+    echo "usage: ./mean.sh <column> [file.csv]" >&2
+    exit 1
+fi
+
+COLUMN=$1
+FILE_PATH=${2:-/dev/stdin}
+
+if [ "$FILE_PATH" != "/dev/stdin" ] && [ ! -r "$FILE_PATH" ]; then
+    echo "Error" >&2
+    exit 1
+fi
+
+cut -d ',' -f "$COLUMN" "$FILE_PATH" | tail -n +2 | {
+    sum=0
+    count=0
+    while read val; do
+        if [[ -n "$val" ]]; then
+            sum=$(echo "$sum + $val" | bc -l)
+            count=$((count + 1))
+        fi
+    done
+
+    if [ $count -gt 0 ]; then
+	echo "$sum / $count" | bc -l
+    else
+	echo "0"
+    fi
+}
+    
